d7c749af8fdc10837cf6d31c57e2d5cd4f6f21ab
kzif93 <kzif93@gmail.com>
2025-04-30 12:29:34 +0200

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,8 +1,8 @@
-from sklearn.feature_selection import VarianceThreshold
 import pandas as pd
+from sklearn.feature_selection import VarianceThreshold
 
 def reduce_low_variance_features(X, threshold=0.01):
     selector = VarianceThreshold(threshold)
     X_reduced = selector.fit_transform(X)
-    selected_cols = X.columns[selector.get_support()]
-    return pd.DataFrame(X_reduced, columns=selected_cols)
+    selected_columns = X.columns[selector.get_support()]
+    return pd.DataFrame(X_reduced, columns=selected_columns, index=X.index)
