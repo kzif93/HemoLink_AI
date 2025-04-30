5bbd1b148577f7d391ae185453f5bb3812657d64
kzif93 <kzif93@gmail.com>
2025-04-30 12:23:55 +0200

    Update explainability.py

--- src/explainability.py
+++ src/explainability.py
@@ -5,6 +5,5 @@ import matplotlib.pyplot as plt
 def show_shap_summary(model, X_test):
     explainer = shap.Explainer(model)
     shap_values = explainer(X_test)
-    st.set_option('deprecation.showPyplotGlobalUse', False)
     shap.summary_plot(shap_values, X_test, plot_type="bar", show=False)
     st.pyplot(bbox_inches='tight')
